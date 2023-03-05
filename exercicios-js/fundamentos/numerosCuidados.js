044d3f05d63401948a315bd7bf0a21f0e8f4367d
Marco AC <marco12125467@gmail.com>
2023-03-05 02:10:41 -0300

    Até a aula 44

--- exercicios-js/fundamentos/numerosCuidados.js
+++ exercicios-js/fundamentos/numerosCuidados.js
@@ -1,6 +1,8 @@
 console.log(7 / 0);
 console.log("10" / 2); // Se o valor pode ser convertido para um valor numérico, ele realiza a operação normalmente.
 console.log("Show!" * 2);
+console.log('5' + 2); // string ganha preferência
+console.log('5' - 2);
 console.log(0.1 + 0.7);
 // forma errada: console.log(10.toString());
 console.log((10.345).toFixed(2));
---
+++ exercicios-js/fundamentos/template-string.js
@@ -0,0 +1,11 @@
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!';
+const template = `
+    Olá
+    ${nome}!`;
+console.log(concatenacao, template);
+
+console.log(`1 + 1 = ${1 +1}`);
+
+const up = texto => texto.toUpperCase();
+console.log(`ei... ${up('cuidado')}!`);
